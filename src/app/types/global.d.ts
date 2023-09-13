50d82b64ad13b8a9a16dd1e4f77eba3b6d1e0e52
Den-codern <78246895+Den-codern@users.noreply.github.com>
2023-09-13 11:57:50 +0600

    fix global declarations

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,6 +1,5 @@
+import * as Yup from "yup";
 declare const _BASE_API_URL_: string;
-import * as Yup from 'Yup';
-
 declare module "yup" {
   interface StringSchema extends Yup.StringSchema {
     phone(): StringSchema;
