9dcfb7cf6af572f2cfa13f14cf9d521a557ef52a
aeshevdaniyar <denaehsev@gmail.com>
2023-10-22 21:19:36 +0600

    fix lint

--- src/pages/AddObjectStepperPage/ui/ProtectAddObjectRoute.tsx
+++ src/pages/AddObjectStepperPage/ui/ProtectAddObjectRoute.tsx
@@ -5,16 +5,15 @@ import { Navigate } from "react-router-dom";
 export const ProtectAddObjectRoute: FC<PropsWithChildren> = (props) => {
   const { children } = props;
 
-  const { city, country, object, region, objectType } = useAppSelector(
-    (state) => state.addObjectForm
-  );
+  const { city, country, objectTypeProperty, region, objectType } =
+    useAppSelector((state) => state.addObjectForm);
 
   if (
     city != undefined &&
     country != undefined &&
-    object != undefined &&
+    objectType != undefined &&
     region != undefined &&
-    objectType != undefined
+    objectTypeProperty != undefined
   ) {
     return <>{children}</>;
   }
